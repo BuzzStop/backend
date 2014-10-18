2341c0551662df05e0ebc243128646416f5f573c
Joel Hoskin <joelhoskin@hotmail.com>
2014-10-18 13:46:52 +0100

    Added some route data

---
+++ route.rb
@@ -0,0 +1,4 @@
+route = [{name: "Queensway Moor Street Link Bus Stop", loc: {lat:52.477815, long:-1.896033},
+         {name: "New Street - Corporation St Intersection (Primark)", loc: {lat:52.479072, long:-1.897813},
+         {name: "Muji", loc: {lat:52.479134, long:-1.898489},
+         {name: "The Studio", loc: {lat:52.479672, long:-1.898164}]
