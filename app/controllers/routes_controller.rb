fb85985f80e589e28fa7527a71a1944e1f581294
Daniel Hollands <daniel@limeblast.co.uk>
2014-10-18 15:15:37 +0100

    Omar to the rescue

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -64,7 +64,7 @@ class RoutesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_route
-      @route = Route.find(params[:id])
+      @route = Route.references(:stop).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/views/routes/show.json.jbuilder
+++ app/views/routes/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.extract! @route, :id, :name, :created_at, :updated_at
+json.merge!(:stops => @route.stops.map(&:attributes))
